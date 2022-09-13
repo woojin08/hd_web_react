76e28f0696ba3b789bd1f75f7d0c78196f0b864a
kimwoojin <kimwoojin08@naver.com>
2022-09-13 16:06:22 +0900

    20220913

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './component/Header';
+import MainVisual from './component/MainVisual';
+
+const App = () => {
+
+  return (
+    <div className="Wrap">
+      <Header />
+      <main>
+        <MainVisual />
+      </main>
+    </div >
+  );
+}
+
+export default App;
